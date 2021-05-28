955966e7d5614acc569ad291a248f971b2e545fa
nipendra <nipendra.dakshana15@gmail.com>
2021-05-29 00:33:46 +0530

    Add: fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import {getProducts} from './services/productService';
 
- const products = [
-    {
-      "id": 1,
-      "category": "shoes",
-      "image": "shoe1.jpg",
-      "name": "Hiker",
-      "price": 94.95,
-      "skus": [
-        { "sku": "17", "size": 7 },
-        { "sku": "18", "size": 8 }
-      ],
-      "description": "This rugged boot will get you up the mountain safely."
-    },
-    {
-      "id": 2,
-      "category": "shoes",
-      "image": "shoe2.jpg",
-      "name": "Climber",
-      "price": 78.99,
-      "skus": [
-        { "sku": "28", "size": 8 },
-        { "sku": "29", "size": 9 }
-      ],
-      "description": "Sure-footed traction in slippery conditions."
-    },
-    {
-      "id": 3,
-      "category": "shoes",
-      "image": "shoe3.jpg",
-      "name": "Explorer",
-      "price": 145.95,
-      "skus": [
-        { "sku": "37", "size": 7 },
-        { "sku": "38", "size": 8 },
-        { "sku": "39", "size": 9 }
-      ],
-      "description": "Look stylish while stomping in the mud."
-    }
-  
-  ];
-
+ 
 export default function App() {
   const [size, setSize] = useState("");
   // cost state = useSate("");
   // size = state[0];
   // setSize = state[1];
+  const [products, setproducts] = useState([]);
 
+  useEffect(()=>{
+    getProducts("shoes").then((response) => setproducts(response));
+  },[]);
 
   function renderProduct(p) {
     return (
@@ -82,6 +47,7 @@ export default function App() {
               <option value="8">8</option>
               <option value="9">9</option>
             </select>
+            {size && <h2>Found {filteredProducts.length} items</h2>}
           </section>
           <section id="products">
             {filteredProducts.map(renderProduct)}
