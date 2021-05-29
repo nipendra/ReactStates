f367bdaa886886832e57dc5d16b2cc5f9eed6d7d
nipendra <nipendra.dakshana15@gmail.com>
2021-05-30 00:45:30 +0530

    Add: react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ErrorBoundry from './ErrorBoundry'
-
+import ErrorBoundry from './ErrorBoundry';
+import {BrowserRouter} from 'react-router-dom';
 ReactDOM.render(
 <ErrorBoundry>
-        <App />
+        <BrowserRouter>
+                <App />
+        </BrowserRouter>
 </ErrorBoundry>, document.getElementById("root")
 );
